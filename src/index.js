bec1e339869b40641effca3a7c1f44216b70578e
M.Kristóf <drzero123456@gmail.com>
2022-06-12 20:59:30 +0200

    branch testing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./styles/style.css"
 import {Todo,Project} from "./functions/todoClasses.js"
 import {getTodos, getProjects, resetStorage} from "./functions/storage.js"
 
+// lel
+
 document.addEventListener("DOMContentLoaded", () => {
 })
 console.log(`Projects: ${JSON.parse(getTodos())}`)
