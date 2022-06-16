deaceeeef46c28c526dad7cca2bc09c76c11ebaf
M.Kristóf <drzero123456@gmail.com>
2022-06-16 20:57:19 +0200

    f

--- src/functions/todoClasses.js
+++ src/functions/todoClasses.js
@@ -1,23 +1,28 @@
-// localStorage.setItem("Projects", JSON.stringify([]))
+
 
 const Project = (name) => {
 
-    function getProjects() {
-        return JSON.parse(localStorage.getItem("Projects"))
+    let project_todos = [];
+
+    const addProject = () => {
+        if (localStorage.getItem("Projects") == null) {
+            localStorage.setItem("Projects", "[]")
+        }
+
+        const project_storage = JSON.parse(localStorage.getItem("Projects"))
+        const project =  {name,project_todos}
+        project_storage.push(project)
+        localStorage.setItem("Projects", JSON.stringify(project_storage))
     }
 
-    return {name};
-
-
+    return {name, project_todos, addProject};
 }
 
-const Todo = (title,description,priority) => {
 
-    function getTodos() {
-        return JSON.parse(localStorage.getItem("Todos"))
-    }
+const Todo = (title,description,date,priority,status="unfinised") => {
 
-    return {title,description,priority}
+
+    return {title,description,date,priority,status}
 }
 
 export {Project,Todo}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import {getTodos, getProjects, resetStorage} from "./functions/storage.js"
 // lel
 
 document.addEventListener("DOMContentLoaded", () => {
+    resetStorage();
+
+    const project1 = Project("Project1");
+    const project2 = Project("Project2");
+
+    project1.addProject();
+    project2.addProject();
 })
 console.log(`Projects: ${JSON.parse(getTodos())}`)
 console.log("Testing")
